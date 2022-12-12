5392c03cdf5204a7ed76cf0f2a58ee8fe3d0c4aa
Josh Kodroff <josh@pulumi.com>
2022-12-12 13:09:45 -0500

    Got public subnet routes working to firewall (programmatically).

--- python/hub.py
+++ python/hub.py
@@ -5,6 +5,8 @@ import pulumi
 import pulumi_aws as aws
 import pulumi_awsx as awsx
 
+from pprint import pprint
+
 
 @dataclass
 class HubVpcArgs:
@@ -13,6 +15,7 @@ class HubVpcArgs:
     tgw_id: pulumi.Input[str]
     spoke_tgw_route_table_id: pulumi.Input[str]
     hub_tgw_route_table_id: pulumi.Input[str]
+    firewall_policy_arn: pulumi.Input[str]
 
 
 class HubVpc(pulumi.ComponentResource):
@@ -20,8 +23,8 @@ class HubVpc(pulumi.ComponentResource):
         super().__init__("awsAdvancedNetworkingWorkshop:index:HubVpc", name, None, opts)
 
         # So we can reference later in our apply handler:
-        self._name = name
-        self._args = args
+        self.name = name
+        self.args = args
 
         self.vpc = awsx.ec2.Vpc(
             f"{name}-vpc",
@@ -61,7 +64,7 @@ class HubVpc(pulumi.ComponentResource):
             ),
         )
 
-        nat_gateway = aws.ec2.NatGateway(
+        self.nat_gateway = aws.ec2.NatGateway(
             f"{name}-nat-gateway",
             aws.ec2.NatGatewayArgs(
                 subnet_id=self.vpc.public_subnet_ids[0],
@@ -72,13 +75,10 @@ class HubVpc(pulumi.ComponentResource):
             )
         )
 
-        self.vpc.isolated_subnet_ids.apply(
-            lambda ids: self.create_nat_routes(ids, nat_gateway.id))
-
         self.tgw_attachment = aws.ec2transitgateway.VpcAttachment(
             f"{name}-tgw-vpc-attachment",
             aws.ec2transitgateway.VpcAttachmentArgs(
-                transit_gateway_id=self._args.tgw_id,
+                transit_gateway_id=self.args.tgw_id,
                 subnet_ids=self.vpc.isolated_subnet_ids,
                 vpc_id=self.vpc.vpc_id,
                 transit_gateway_default_route_table_association=False,
@@ -97,7 +97,6 @@ class HubVpc(pulumi.ComponentResource):
             )
         )
 
-        # TODO: Fix the name of this route.
         aws.ec2transitgateway.Route(
             f"{name}-default-spoke-to-inspection",
             aws.ec2transitgateway.RouteArgs(
@@ -121,8 +120,21 @@ class HubVpc(pulumi.ComponentResource):
             ),
         )
 
-        self.vpc.public_subnet_ids.apply(
-            lambda x: self.create_inbound_routes(x))
+        self.vpc.isolated_subnet_ids.apply(
+            lambda ids: self.create_nat_routes(ids, self.nat_gateway.id))
+
+        self.create_firewall()
+
+        pulumi.Output.all(self.firewall.firewall_statuses,
+                          self.vpc.public_subnet_ids).apply(lambda args: self.create_inbound_routes_to_firewall(args[0], args[1]))
+
+        # These need to be commented out before the call above to
+        # create_inbound_routes_to_firewall can be made. We have conflicting
+        # routes and therefore need to tear down the old before we create the
+        # new.
+
+        # self.vpc.public_subnet_ids.apply(
+        #     lambda x: self.create_inbound_routes_to_tgw(x))
 
         # inspection_subnets.apply(lambda x: self._create_outbound_routes(x.ids))
 
@@ -132,6 +144,7 @@ class HubVpc(pulumi.ComponentResource):
             # TODO: Check whether this is being returned before it's actually
             # provisioned and is causing an issue downstream if we try to spin
             # this stack all up at once.
+            # (It may also not be a problem.)
             "tgw_attachment": self.tgw_attachment,
         })
 
@@ -143,7 +156,7 @@ class HubVpc(pulumi.ComponentResource):
             )
 
             aws.ec2.Route(
-                f"{self._name}-route-{subnet_id}-to-tgw",
+                f"{self.name}-route-{subnet_id}-to-tgw",
                 aws.ec2.RouteArgs(
                     route_table_id=route_table.id,
                     destination_cidr_block="0.0.0.0/0",
@@ -154,7 +167,7 @@ class HubVpc(pulumi.ComponentResource):
                 ),
             )
 
-    def create_inbound_routes(self, subnet_ids: Sequence[str]):
+    def create_inbound_routes_to_tgw(self, subnet_ids: Sequence[str]):
         '''Creates routes for the supernet (a CIDR block that encompasses all
         spoke VPCs) from the public subnets in the hub VPC (where the NAT
         Gateways for centralized egress live) to the TGW'''
@@ -164,14 +177,146 @@ class HubVpc(pulumi.ComponentResource):
             )
 
             aws.ec2.Route(
-                f"{self._name}-route-{subnet_id}-to-tgw",
+                f"{self.name}-route-{subnet_id}-to-tgw",
                 aws.ec2.RouteArgs(
                     route_table_id=route_table.id,
-                    destination_cidr_block=self._args.supernet_cidr_block,
-                    transit_gateway_id=self._args.tgw_id,
+                    destination_cidr_block=self.args.supernet_cidr_block,
+                    transit_gateway_id=self.args.tgw_id,
                 ),
                 pulumi.ResourceOptions(
                     depends_on=[self.tgw_attachment],
+                    parent=self,
+                ),
+            )
+
+    def create_firewall(self):
+        region = aws.config.region
+        inspection_subnets = [
+            {"az": f"{region}a", "cidr": "10.129.0.32/28"},
+            {"az": f"{region}b", "cidr": "10.129.0.96/28"},
+            {"az": f"{region}c", "cidr": "10.129.0.160/28"},
+        ]
+        subnet_ids = []
+        for i, inspection_subnet in enumerate(inspection_subnets):
+            resource_name = f"{self.name}-inspection-{i+1}"
+            subnet = aws.ec2.Subnet(
+                resource_name,
+                aws.ec2.SubnetArgs(
+                    vpc_id=self.vpc.vpc_id,
+                    availability_zone=inspection_subnet["az"],
+                    cidr_block=inspection_subnet["cidr"],
+                    tags={
+                        "Name": resource_name
+                    }
+                ),
+                opts=pulumi.ResourceOptions(
+                    parent=self,
+                    # This makes it easier to avoid CIDR block conflicts:
+                    delete_before_replace=True,
+                ),
+            )
+
+            subnet_ids.append(subnet.id)
+
+            route_table = aws.ec2.RouteTable(
+                resource_name,
+                aws.ec2.RouteTableArgs(
+                    vpc_id=self.vpc.vpc_id,
+                    tags={
+                        "Name": resource_name,
+                    }
+                ),
+                opts=pulumi.ResourceOptions(
+                    parent=subnet,
+                ),
+            )
+
+            aws.ec2.RouteTableAssociation(
+                resource_name,
+                aws.ec2.RouteTableAssociationArgs(
+                    route_table_id=route_table.id,
+                    subnet_id=subnet.id
+                ),
+                opts=pulumi.ResourceOptions(
+                    parent=subnet,
+                ),
+            )
+
+            aws.ec2.Route(
+                f"{self.name}-insp-supernet-to-tgw-{i+1}",
+                aws.ec2.RouteArgs(
+                    route_table_id=route_table.id,
+                    destination_cidr_block=self.args.supernet_cidr_block,
+                    transit_gateway_id=self.args.tgw_id
+                ),
+                opts=pulumi.ResourceOptions(
+                    parent=route_table,
+                ),
+            )
+
+            # TODO: Consider whether we should delete this.
+            # This may not be necessary - it's unlikely we would place hosts in
+            # this subnet, much less ones that would need internet access.
+            aws.ec2.Route(
+                f"{self.name}-insp-default-to-nat-{i+1}",
+                aws.ec2.RouteArgs(
+                    route_table_id=route_table.id,
+                    destination_cidr_block="0.0.0.0/0",
+                    nat_gateway_id=self.nat_gateway.id
+                ),
+                opts=pulumi.ResourceOptions(
+                    parent=route_table,
+                ),
+
+            )
+
+        subnet_mappings = list(map(lambda id: {"subnet_id": id}, subnet_ids))
+
+        self.firewall = aws.networkfirewall.Firewall(
+            f"{self.name}-firewall",
+            aws.networkfirewall.FirewallArgs(
+                vpc_id=self.vpc.vpc_id,
+                firewall_policy_arn=self.args.firewall_policy_arn,
+                subnet_mappings=subnet_mappings,
+            ),
+            opts=pulumi.ResourceOptions(
+                parent=self,
+            ),
+        )
+
+    def create_inbound_routes_to_firewall(self, statuses, subnet_ids):
+        # Map the output of the Firewall attachments to a structure more
+        # suitable structure:
+        attachments = []
+        for sync_state in statuses[0]["sync_states"]:
+            attachment = {
+                "az": sync_state["availability_zone"],
+                "subnet_id": sync_state["attachments"][0]["subnet_id"],
+                "endpoint_id": sync_state["attachments"][0]["endpoint_id"],
+            }
+            attachments.append(attachment)
+
+        for subnet_id in subnet_ids:
+            subnet = aws.ec2.get_subnet(id=subnet_id)
+            route_table = aws.ec2.get_route_table(
+                subnet_id=subnet_id
+            )
+
+            # Find the attachment in our availability zone
+            subnet_attachments = [
+                attachment for attachment in attachments if attachment['az'] == subnet.availability_zone]
+            if len(subnet_attachments) != 1:
+                raise Exception(
+                    f"Expected exactly 1 firewall subnet attachment for AZ '{subnet.availability_zone}'. Found {len(subnet_attachments)} instead.")
+
+            aws.ec2.Route(
+                f"{self.name}-{subnet_id}-to-firewall",
+                aws.ec2.RouteArgs(
+                    route_table_id=route_table.id,
+                    destination_cidr_block=self.args.supernet_cidr_block,
+                    vpc_endpoint_id=subnet_attachments[0]["endpoint_id"],
+                ),
+                pulumi.ResourceOptions(
                     parent=self,
                 ),
             )
